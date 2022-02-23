31f21956c61e2c06b57932bd0f02bf82e84b120c
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2022-02-22 23:32:22 -0600

    Updated Conditionals

--- app.py
+++ app.py
@@ -81,11 +81,12 @@ def pizzafun():
 
     items = request.json["items"]
     visitor = request.json["visitor"]
+    body = visitor["body"]
     city = visitor["city"]
     #implementing objects to call them
     if check_ny(city):
         check_main_msgs(items, visitor["emailAddress"], visitor["phoneNumber"])
-    elif check_msg_body(items):
+    elif check_msg_body(body):
         check_main_msgs(items, visitor["emailAddress"], visitor["phoneNumber"])
         return saladlover()
     else:
