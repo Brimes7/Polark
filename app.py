2967499710883160eaa15db96a2f3ec3a0181fec
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2022-02-22 23:26:37 -0600

    Updated Conditionals

--- app.py
+++ app.py
@@ -85,7 +85,8 @@ def pizzafun():
     #implementing objects to call them
     if check_ny(city):
         check_main_msgs(items, visitor["emailAddress"], visitor["phoneNumber"])
-    elif check_msg_body(items) != "pizza":
+    elif check_msg_body(items):
+        check_main_msgs(items, visitor["emailAddress"], visitor["phoneNumber"])
         return saladlover()
     else:
         return notny()
