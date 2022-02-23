641cff8970e43fe82926001a302773ebc1947fbc
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2022-02-22 23:18:14 -0600

    Updated Conditionals

--- app.py
+++ app.py
@@ -33,16 +33,16 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return "HELLO"
+    return "HELLO NEW YORK CITY!"
 
-
+#Checks the body
 def check_msg_body(body):
     return "pizza" in body.lower()
 
-
+#Checks the kind
 def check_msg_kind(kind):
     return kind == "MessageToOperator"
-
+#Checks the main messages
 def check_main_msgs(msgs, emailAddress, phoneNumber):
     for msg in msgs:
 
@@ -52,7 +52,7 @@ def check_main_msgs(msgs, emailAddress, phoneNumber):
         if check_msg_kind(kind) and check_msg_body(body):
             send_pizza_req(emailAddress, phoneNumber)
 
-
+#Check if even in NY
 def check_ny(city):
     return "new york" in city.lower()
 
@@ -61,7 +61,16 @@ def notny():
     sys.stdout.flush()
     if request.method == 'POST':
         print(request.json)
-        return"CUSTOMER NOT IN NEW YORK AREA"
+        return"CUSTOMER NOT IN NEW YORK AREA. INELLIGBLE FOR PIZZA ROLLOUT."
+    else:
+        abort(400)
+
+def saladlover():
+    print("\n\nReceived")
+    sys.stdout.flush()
+    if request.method == 'POST':
+        print(request.json)
+        return "CUSTOMER NEVER MENTIONED A PIZZA"
     else:
         abort(400)
 
@@ -73,33 +82,15 @@ def pizzafun():
     items = request.json["items"]
     visitor = request.json["visitor"]
     city = visitor["city"]
-    #not a string until you pull it out || Still an object
+    #implementing objects to call them
     if check_ny(city):
         check_main_msgs(items, visitor["emailAddress"], visitor["phoneNumber"])
+        if check_msg_body(items) != "pizza":
+            return saladlover()
     else:
         return notny()
 
-    return 'PIZZA TIME'
-# THIS IS THE LISTENER
-# @app.route('/custom', methods=['POST'])
-# def webhook():
-#     print("\n\nReceived")
-#     sys.stdout.flush()
-#     if request.method == 'POST':
-#         print(request.json)
-#         return 'NEW WEBHOOK WORKS', 200
-#     else:
-#         abort(400)
-
-
-def saladlover():
-    print("\n\nReceived")
-    sys.stdout.flush()
-    if request.method == 'POST':
-        print(request.json)
-        return "CUSTOMER NEVER MENTIONED A PIZZA"
-    else:
-        abort(400)
+    return 'IT IS PIZZA TIME'
 
 if __name__ == '__main__':
     app.run(debug=True)
