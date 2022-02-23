5b8cea64232ae5eec1d5ab458d98572ce2dba09c
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2022-02-22 20:29:17 -0600

    Updated functionality

--- nypizza.py
+++ nypizza.py
@@ -11,19 +11,41 @@ PARAMS =  {
            "pizza-size": "small",
            "topping": "cheese"
         }
-r = requests.get(url = URL, params = PARAMS)
-# That one  will only pull from local server not our URL
-# r = requests.post(polark_url, data=json.dumps(data ), headers={"Content-Type": 'application/json'})
-data = r.json()
-print(PARAMS)
+# r = requests.get(url = URL, params = PARAMS)
+# That one will only pull from local server not our URL
+# r = requests.post(URL, data=json.dumps(PARAMS), headers={"Content-Type": 'application/json'})
+# data = r.json()
+# print(PARAMS)
+def send_pizza_req(email, phone):
+    params = {
+        "email": email,
+        "phone": phone,
+        "pizza-size": "small",
+        "topping": "cheese"
+    }
 
+    r = requests.post(URL, data=json.dumps(params), headers={"Content-Type": 'application/json'})
+    print(f"SENT PIZZA REQUEST FOR {email}")
 
 nypizza = Flask(__name__)
 # Intitiates the listener
 @nypizza.route('/', methods=['POST'])
 def pizzafun():
-    return 'PIZZA TIME'
 
+    items = request.json["items"]
+    visitor = request.json["visitor"]
+    for msg in items:
+        print(msg)
+        body = msg["body"]
+        print(body)
+        kind = msg["kind"]
+        if kind == "MessageToOperator":
+            if "pizza" in body:
+                send_pizza_req(visitor["emailAddress"], visitor["phoneNumber"])
+
+
+
+    return 'PIZZA TIME'
 # THIS IS THE LISTENER
 @nypizza.route('/custom', methods=['POST'])
 def webhook():
